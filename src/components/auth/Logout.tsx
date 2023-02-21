43268abb36e3017aad62049d18dd11d3ed004a1d
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2023-02-21 13:50:24 +0530

    url hitting infinite times resolved

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -5,14 +5,12 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../../config/firebase";
 import { useDispatch } from "react-redux";
 import { clearUser } from "../../reducers/userReducer";
-import { GoogleAuthProvider } from "firebase/auth";
 interface Props {
   navigateTo?: string;
 }
 
 const Logout = ({ navigateTo = "/login" }: Props) => {
   const [disabled, setDisabled] = useState(false);
-  const provider = new GoogleAuthProvider();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
